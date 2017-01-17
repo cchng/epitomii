909192d598e3312ddeeb237baf89c87ed6c2e7f1
Carolyn Ch'ng <carolyn.l.chng@gmail.com>
2017-01-16 21:19:05 -0800

    updated jam

--- epitomii.py
+++ epitomii.py
@@ -5,7 +5,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, make_response, Markup
 
 
-YOUTUBE_CODE = "ZuRl-E7cUug"
+YOUTUBE_CODE = "qmbX3odL5xg"
 DAILY_JAM = "https://www.youtube.com/embed/{code}?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&loop=1&playlist={code}".format(code=YOUTUBE_CODE)
 #DAILY_JAM = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/264414483&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
 
